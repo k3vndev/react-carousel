28cc81a97a79435e57627e0decd5342b640c1c4a
K3vnDev <kevinrpolo16@gmail.com>
2026-03-09 13:26:58 -0500

    Web: Refactor Hero and HeroCarousel components for improved structure and responsiveness; update text display for mobile and desktop

--- web/src/components/hero/HeroCarousel.tsx
+++ web/src/components/hero/HeroCarousel.tsx
@@ -6,24 +6,26 @@ export const HeroCarousel = () => {
   const catImages = useMemo(() => getCatImages('hero', 7), [])
 
   return (
-    <Carousel
-      className='md:w-3xl w-full px-8 animate-slide-in-b anim-blur-xl anim-opacity-0 anim-ease-out-back anim-delay-500 anim-rotate-5'
-      navigationHandler={
-        <NavigationDots
-          className='[&>.dot.active]:bg-yellow-400'
-          autoScrollAnimationValues={{
-            color: 'oklch(85.2% 0.199 91.936)'
-          }}
-        />
-      }
-      autoScroll
-      infiniteScroll
-    >
-      {catImages.map((src, i) => (
-        <CarouselItem key={i} className='h-96'>
-          <img src={src} alt={`Cat ${i + 1}`} className='w-full h-full object-cover' draggable={false} />
-        </CarouselItem>
-      ))}
-    </Carousel>
+    <div className='w-full flex justify-center h-content px-8 pb-20'>
+      <Carousel
+        className='md:w-3xl w-full animate-slide-in-b anim-blur-xl anim-opacity-0 anim-ease-out-back anim-delay-500 anim-rotate-5'
+        navigationHandler={
+          <NavigationDots
+            className='[&>.dot.active]:bg-yellow-400'
+            autoScrollAnimationValues={{
+              color: 'oklch(85.2% 0.199 91.936)'
+            }}
+          />
+        }
+        autoScroll
+        infiniteScroll
+      >
+        {catImages.map((src, i) => (
+          <CarouselItem key={i} className='sm:h-96 h-64'>
+            <img src={src} alt={`Cat ${i + 1}`} className='w-full h-full object-cover' draggable={false} />
+          </CarouselItem>
+        ))}
+      </Carousel>
+    </div>
   )
 }
